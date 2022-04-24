2358fbdfc9ccd602ae7701525ca1d82603a8fad5
unknown <asifshezan7@gmail.com>
2022-04-24 16:38:02 +0600

    data function & if-else & button toggle & hide-show

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,29 @@
 <template>
     <div>
-        <h1>{{ data }}</h1>
-        <h2>{{ msg }}</h2>
+        <!-- <h1>{{ data }}</h1>-->
+        <h2 v-if="show">Condition in Vue.Js</h2>
+        <h2 v-else>Condition in Else block</h2>
+        <button v-on:click="hi">Show Hi Function</button>
+        <button v-on:mouseover="hi">Show Hi Function</button>
+        <button v-on:click="display">Toggle Element</button>
     </div>
 </template>
 <script>
 export default {
+    // eslint-disable-next-line vue/multi-word-component-names
     name:'Home',
     props:{
         data:String,
-        msg:String,
+        // msg:String,
+    },methods: {
+        hi(){
+            console.log("Everything is alright.")
+            alert("I want to get up in this world.")
+        },display(){
+            this.show=!this.show
+        }
+    },data(){
+        return {show:true}
     }
 };
 </script>
